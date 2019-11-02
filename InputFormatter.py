10bd6b31c63c191451275ef2697e5f34556c1c2c
Kellan Cupid <37488030+SirKupid@users.noreply.github.com>
2019-11-02 18:47:52 -0400

    Adds punctuation to the end of the line

--- InputFormatter.py
+++ InputFormatter.py
@@ -1,5 +1,6 @@
 import string
 import pandas as pd
+import re
 
 df = pd.read_csv("outputwithheader.csv", header=[0])
 oldArr  = ['Nearly   Fired TWICE to Winning The SEC! | Mizzou 5-Year Rebuild | NCAA Football 14 (69/126)',
@@ -15,13 +16,24 @@ def myfunc(a):
 
 for i in range(df.shape[0]):
     w = df.iloc[i].title
+    df.at[i, 'title'] = w
+    p = {}
+    for j in re.findall("\W", w):
+        if j == " ":
+            continue
+        elif j in p:
+            p[j] += 1
+        else:
+            p[j] = 1
     for x in string.punctuation:
         w = w.replace(x, " ")
     a = w.split(' ')
     a = filter(myfunc, a)
     w = ' '.join(a)
-    df.at[i, 'title'] = w
+    for k in p.keys():
+        w += " " + (k * p[k])
 
     print(w)
 df.to_csv("formttedInput.csv")
 df.to_json("formattedInput.json", orient='records')
+
